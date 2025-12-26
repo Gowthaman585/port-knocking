ef66f3d79d05f52813846e1a7b0039c2875c359a
Gowthaman M <gowthamanm585@gmail.com>
2025-12-26 20:21:43 +0530

    Updated iptables-cmds

--- iptable-rules.sh
+++ iptable-rules.sh
@@ -8,3 +8,7 @@ sudo iptables -A INPUT -i lo -j ACCEPT
 
 #Drop any other incomming packets
 sudo iptables -A INPUT -j DROP
+
+#Instructing user to start knockd daemon
+echo "Start knockd daemon by sudo systemctl start knockd"
+echo "If any error occures please revisit README.md"
